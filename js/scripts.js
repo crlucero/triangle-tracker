442eb5482bfe1f41d8717d3f011f58e3938b38ec
Cristian Lucero and Kenny Wolfenberger <kenny@>
2018-10-18 14:20:16 -0700

    updated files

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,32 @@ $(document).ready(function() {
   $(".formOne").submit(function(event) {
   event.preventDefault();
 
-  var sideA = $("#sideA").val();
-  var sideB = $("#sideB").val();
-  var sideC = $("#sideC").val();
+  var sideA = parseInt($("#sideA").val());
+  var sideB = parseInt($("#sideB").val());
+  var sideC = parseInt($("#sideC").val());
 
 
   if ( sideA === sideB && sideA === sideC ) {
     $(".equilateral").show();
   }
-   if (sideA === sideB && sideA != sideC, sideA === sideC && sideA != sideB ) {
+   if (sideA === sideB && sideA != sideC) {
     $(".isosceles").show();
   }
-
-
-  if (sideA != sideB && sideA != sideC) {
+if (sideA === sideC && sideA != sideB ) {
+  $(".isosceles").show();
+}
+  if (sideA !== sideB && sideA !== sideC && sideB !== sideC) {
     $(".scalene").show();
   }
-
-
+if ((sideA) + (sideB) <= sideC){
+    $(".not-triangle").show();
+}
+if ((sideB) + (sideC) <= sideA){
+    $(".not-triangle").show();
+}
+if ((sideA) + (sideC) <= sideB){
+    $(".not-triangle").show();
+}
 });
 
 });
